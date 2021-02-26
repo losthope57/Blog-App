467aad17faf1716980f8af171dda1e5aac19e176
simonasjur <simonasjur@gmail.com>
2021-02-26 19:32:03 +0200

    updated

---
+++ Blog Api/Controllers/BlogPostsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Blog_Api.Models;
+using AutoMapper;
+
+namespace Blog_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly BlogContext _context;
+        private readonly IMapper _mapper;
+
+        public BlogPostsController(BlogContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/BlogPosts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BlogPost>>> GetBlogPosts()
+        {
+            return await _context.BlogPosts.ToListAsync();
+        }
+
+        // GET: api/BlogPosts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BlogPost>> GetBlogPost(long id)
+        {
+            var blogPost = await _context.BlogPosts.FindAsync(id);
+
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return blogPost;
+        }
+
+        // PUT: api/BlogPosts/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBlogPost(long id, BlogPost blogPost)
+        {
+            if (id != blogPost.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(blogPost).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BlogPostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BlogPosts
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<BlogPost>> PostBlogPost(BlogPost blogPost)
+        {
+            _context.BlogPosts.Add(blogPost);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBlogPost", new { id = blogPost.Id }, blogPost);
+        }
+
+        // DELETE: api/BlogPosts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BlogPost>> DeleteBlogPost(long id)
+        {
+            var blogPost = await _context.BlogPosts.FindAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            _context.BlogPosts.Remove(blogPost);
+            await _context.SaveChangesAsync();
+
+            return blogPost;
+        }
+
+        private bool BlogPostExists(long id)
+        {
+            return _context.BlogPosts.Any(e => e.Id == id);
+        }
+    }
+}
--- Blog Api/Startup.cs
+++ Blog Api/Startup.cs
@@ -1,15 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
+using Blog_Api.Helpers;
+using Blog_Api.Models;
+using Blog_Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Blog_Api
 {
@@ -25,7 +34,59 @@ namespace Blog_Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddDbContext<BlogContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("BlogConnection")));
             services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = "Swagger Demo API",
+                        Description = "Demo API for showing Swagger",
+                        Version = "v1"
+                    });
+
+                var fileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+                options.IncludeXmlComments(filePath);
+            });
+
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<IUserService, UserService>();
+
+
+
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,11 +101,20 @@ namespace Blog_Api
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo API");
+                options.RoutePrefix = "";
             });
         }
     }
